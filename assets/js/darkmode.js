6fe86e81be4b2ac140fd6b637b8bc2daff2ca090
Nabirah Siddiqui <actuarydne@gmail.com>
2024-10-22 00:13:43 +0500

    update

--- assets/js/darkmode.js
+++ assets/js/darkmode.js
@@ -5,13 +5,11 @@ const logo = document.querySelector('.navbar-brand-icon')
 const enableDarkmode = () => {
     document.body.classList.add('darkmode')
     localStorage.setItem('darkmode','active')
-    logo.src = 'assets/images/PURRFECT-rresume---darkmode-2.png'
 }
 
 const disableDarkmode = () => {
     document.body.classList.remove('darkmode')
     localStorage.setItem('darkmode', null)
-    logo.src = '/assets/images/PURRFECT-RESUME.png'
 }
 
 if(darkmode === "active") { 
